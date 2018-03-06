c9368ad1b5c4516aa6e2ed19ac28004d5371029d
Mackenzie <clarkmackenzieb@gmail.com>
2018-03-06 10:30:43 -0600

    finished display on character component with minor styling

--- src/Components/Character.js
+++ src/Components/Character.js
@@ -1,36 +1,42 @@
 import React from "react";
 
+import "./styles.css";
+
 const Character = props => {
   console.log(props);
 
   return (
     <div>
-      <div>
+      <div className="character-container">
         <img
           src={`https://render-us.worldofwarcraft.com/character/${
             props.profile.thumbnail
           }`}
           alt="amilanae-avatar"
+          className="character-avatar"
         />
-        <p>
-          {props.profile.name}, Level {props.profile.level}
-        </p>
-        <p>iLvL {props.items.iLvl}</p>
+        <div className="character-info">
+          <p>
+            {props.profile.name}, Level {props.profile.level}
+          </p>
+          <p>Item Level: {props.items.iLvl}</p>
+        </div>
       </div>
       <div>
         <h3>Gear</h3>
         {props.items &&
           props.items.itemList.map((item, i) => {
             return (
-              <div key={i}>
+              <div className="indiv-item" key={i}>
                 <img
                   src={`https://wow.zamimg.com/images/wow/icons/large/${
                     item.icon
                   }.jpg`}
                   alt="armor-icon"
+                  className="indiv-item-image"
                 />
                 <p>
-                  {item.name}, iLvl {item.itemLevel}
+                  {item.name}, {item.itemLevel}
                 </p>
               </div>
             );
